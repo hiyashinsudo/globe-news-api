25e407c50197d80540ef9c25d86abe41edf72107
Yoshida Yasuhiro <yasushi.adamant@gmail.com>
2022-08-03 02:18:38 +0900

    名称変更、コメント修正

--- resource/query.sql
+++ resource/query.sql
@@ -1,4 +1,4 @@
---ローカルでデータ挿入用のクエリ
+--ニュース記事のテーブル作成
 CREATE TABLE articles (
  id SERIAL primary key,
  title TEXT NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE articles (
  updated_at timestamp with time zone default current_timestamp
 );
 
---ローカルでテストデータ挿入用のクエリ
+--ローカルでテストデータ挿入用
 INSERT INTO articles (title, author, url, urlToImage, description, country, published_At) VALUES ('テストタイトル1','テスト著者1','http://localhost/','http://localhost/img','テスト説明1','jp', '2022-08-01T12:41:03Z');
