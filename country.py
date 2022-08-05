cc3d97ffa90659c10c31e0537c3031e163010462
Yoshida Yasuhiro <yasushi.adamant@gmail.com>
2022-08-05 22:07:44 +0900

    国別コード、国名、緯度経度の構造を含むEnumを定義

--- country.py
+++ country.py
@@ -56,3 +56,276 @@ class Country(Enum):
     US = 'us'
     VE = 've'
     ZA = 'za'
+
+
+class CountryDetail(Enum):
+    AE = {'code': 'ae',
+          'jp_name': 'アラブ首長国連邦',
+          'en_name': 'United Arab Emirates',
+          'lat': '',
+          'long': ''}
+    AR = {'code': 'ar',
+          'jp_name': 'アルゼンチン共和国',
+          'en_name': 'Argentina',
+          'lat': '',
+          'long': ''}
+    AT = {'code': 'at',
+          'jp_name': 'オーストリア',
+          'en_name': 'Austria',
+          'lat': '',
+          'long': ''}
+    AU = {'code': 'au',
+          'jp_name': '	オーストラリア',
+          'en_name': 'Australia',
+          'lat': '',
+          'long': ''}
+    BE = {'code': 'be',
+          'jp_name': 'ベルギー',
+          'en_name': 'Belgium',
+          'lat': '',
+          'long': ''}
+    BG = {'code': 'bg',
+          'jp_name': 'ブルガリア',
+          'en_name': 'Bulgaria',
+          'lat': '',
+          'long': ''}
+    BR = {'code': 'br',
+          'jp_name': 'ブラジル',
+          'en_name': 'Brazil',
+          'lat': '',
+          'long': ''}
+    CA = {'code': 'ca',
+          'jp_name': 'カナダ',
+          'en_name': 'Canada',
+          'lat': '',
+          'long': ''}
+    CH = {'code': 'ch',
+          'jp_name': 'スイス',
+          'en_name': 'Switzerland',
+          'lat': '',
+          'long': ''}
+    CN = {'code': 'cn',
+          'jp_name': '中国',
+          'en_name': 'China',
+          'lat': '',
+          'long': ''}
+    CO = {'code': 'co',
+          'jp_name': 'コロンビア',
+          'en_name': 'Colombia',
+          'lat': '',
+          'long': ''}
+    CU = {'code': 'cu',
+          'jp_name': 'キューバ',
+          'en_name': 'Cuba',
+          'lat': '',
+          'long': ''}
+    CZ = {'code': 'cz',
+          'jp_name': 'チェコ',
+          'en_name': 'Czech Republic',
+          'lat': '',
+          'long': ''}
+    DE = {'code': 'de',
+          'jp_name': 'ドイツ',
+          'en_name': 'Germany',
+          'lat': '',
+          'long': ''}
+    EG = {'code': 'eg',
+          'jp_name': 'エジプト',
+          'en_name': 'Egypt',
+          'lat': '',
+          'long': ''}
+    FR = {'code': 'fr',
+          'jp_name': 'フランス',
+          'en_name': 'France',
+          'lat': '',
+          'long': ''}
+    GB = {'code': 'gb',
+          'jp_name': 'イギリス',
+          'en_name': 'Great Britain',
+          'lat': '',
+          'long': ''}
+    GR = {'code': 'gr',
+          'jp_name': 'ギリシャ',
+          'en_name': 'Greece',
+          'lat': '',
+          'long': ''}
+    HK = {'code': 'hk',
+          'jp_name': '香港',
+          'en_name': 'Hong Kong',
+          'lat': '',
+          'long': ''}
+    HU = {'code': 'hu',
+          'jp_name': 'ハンガリー',
+          'en_name': 'Hungary',
+          'lat': '',
+          'long': ''}
+    ID = {'code': 'id',
+          'jp_name': 'インドネシア',
+          'en_name': 'Indonesia',
+          'lat': '',
+          'long': ''}
+    IE = {'code': 'ie',
+          'jp_name': 'アイルランド',
+          'en_name': 'Ireland',
+          'lat': '',
+          'long': ''}
+    IL = {'code': 'il',
+          'jp_name': 'イスラエル',
+          'en_name': 'Israel',
+          'lat': '',
+          'long': ''}
+    IN = {'code': 'in',
+          'jp_name': 'インド',
+          'en_name': 'India',
+          'lat': '',
+          'long': ''}
+    IT = {'code': 'it',
+          'jp_name': 'イタリア',
+          'en_name': 'Italy',
+          'lat': '',
+          'long': ''}
+    JP = {'code': 'jp',
+          'jp_name': '日本',
+          'en_name': 'Japan',
+          'lat': '',
+          'long': ''}
+    KR = {'code': 'kr',
+          'jp_name': '韓国',
+          'en_name': 'Korea',
+          'lat': '',
+          'long': ''}
+    LT = {'code': 'lt',
+          'jp_name': 'リトアニア',
+          'en_name': 'Lithuania',
+          'lat': '',
+          'long': ''}
+    LV = {'code': 'lv',
+          'jp_name': 'ラトビア',
+          'en_name': 'Latvia',
+          'lat': '',
+          'long': ''}
+    MA = {'code': 'ma',
+          'jp_name': 'モロッコ',
+          'en_name': 'Morocco',
+          'lat': '',
+          'long': ''}
+    MX = {'code': 'mx',
+          'jp_name': 'メキシコ',
+          'en_name': 'Mexico',
+          'lat': '',
+          'long': ''}
+    MY = {'code': 'my',
+          'jp_name': 'マレーシア',
+          'en_name': 'Malaysia',
+          'lat': '',
+          'long': ''}
+    NG = {'code': 'ng',
+          'jp_name': 'ナイジェリア',
+          'en_name': 'Nigeria',
+          'lat': '',
+          'long': ''}
+    NL = {'code': 'nl',
+          'jp_name': 'オランダ',
+          'en_name': 'Netherlands',
+          'lat': '',
+          'long': ''}
+    NO = {'code': 'no',
+          'jp_name': 'ノルウェー',
+          'en_name': 'Norway',
+          'lat': '',
+          'long': ''}
+    NZ = {'code': 'nz',
+          'jp_name': 'ニュジーランド',
+          'en_name': 'New Zealand',
+          'lat': '',
+          'long': ''}
+    PH = {'code': 'ph',
+          'jp_name': 'フィリピン',
+          'en_name': 'Philippines',
+          'lat': '',
+          'long': ''}
+    PL = {'code': 'pl',
+          'jp_name': 'ポーランド',
+          'en_name': 'Poland',
+          'lat': '',
+          'long': ''}
+    PT = {'code': 'pt',
+          'jp_name': 'ポルトガル',
+          'en_name': 'Portugal',
+          'lat': '',
+          'long': ''}
+    RO = {'code': 'ro',
+          'jp_name': 'ルーマニア',
+          'en_name': 'Romania',
+          'lat': '',
+          'long': ''}
+    RS = {'code': 'rs',
+          'jp_name': 'セルビア',
+          'en_name': 'Serbia',
+          'lat': '',
+          'long': ''}
+    RU = {'code': 'ru',
+          'jp_name': 'ロシア',
+          'en_name': 'Russian Federation',
+          'lat': '',
+          'long': ''}
+    SA = {'code': 'sa',
+          'jp_name': 'サウジアラビア',
+          'en_name': 'Saudi Arabia',
+          'lat': '',
+          'long': ''}
+    SE = {'code': 'se',
+          'jp_name': 'スウェーデン',
+          'en_name': 'Sweden',
+          'lat': '',
+          'long': ''}
+    SG = {'code': 'sg',
+          'jp_name': 'シンガポール',
+          'en_name': 'Singapore',
+          'lat': '',
+          'long': ''}
+    SI = {'code': 'si',
+          'jp_name': 'スロベニア',
+          'en_name': 'Slovenia',
+          'lat': '',
+          'long': ''}
+    SK = {'code': 'sk',
+          'jp_name': 'スロバキア',
+          'en_name': 'Slovakia',
+          'lat': '',
+          'long': ''}
+    TH = {'code': 'th',
+          'jp_name': 'トルコ',
+          'en_name': 'Turkey',
+          'lat': '',
+          'long': ''}
+    TR = {'code': 'tr',
+          'jp_name': '',
+          'en_name': '',
+          'lat': '',
+          'long': ''}
+    TW = {'code': 'tw',
+          'jp_name': '台湾',
+          'en_name': 'Taiwan',
+          'lat': '',
+          'long': ''}
+    UA = {'code': 'ua',
+          'jp_name': 'ウクライナ',
+          'en_name': 'Ukraine',
+          'lat': '',
+          'long': ''}
+    US = {'code': 'us',
+          'jp_name': 'アメリカ',
+          'en_name': 'United States',
+          'lat': '',
+          'long': ''}
+    VE = {'code': 've',
+          'jp_name': 'ベネズエラ',
+          'en_name': 'Venezuela',
+          'lat': '',
+          'long': ''}
+    ZA = {'code': 'za',
+          'jp_name': '南アフリカ',
+          'en_name': 'South Africa',
+          'lat': '',
+          'long': ''}
